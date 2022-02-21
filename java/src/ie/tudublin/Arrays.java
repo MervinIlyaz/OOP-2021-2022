31f95e2b06308085d8a6858f1ca13d13d0fc8160
MervinIlyaz <c20371681@mytudublin.ie>
2022-02-21 12:24:01 +0000

    months

---
+++ java/src/ie/tudublin/Arrays.java
@@ -0,0 +1,62 @@
+package ie.tudublin;
+
+import processing.core.PApplet;
+
+public class Arrays extends PApplet{
+    
+
+    float[] rainfall = {45,37,55,27,38,50,79,48,104,51,100,58};
+
+    String[] months = {"Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"};
+    
+    float[] a1 = new float[100];
+    float[] a2;   
+    public void settings(){
+        size(500,500);
+    }
+    public void setup(){
+        for (int i= 0; i < rainfall.length; i++)
+        {
+            println(rainfall[i] + "\t" + months[i]);
+        }
+        for (float r:rainfall)
+        {
+            print(r);
+        }
+
+        int j = 0;
+        for (float r:rainfall)
+        {
+            println(r + "\t" + months[j]);
+            j++;
+        }
+
+        for (int i = rainfall.length - 1; i >=0; i-- )
+        {
+            println(rainfall[i] + "\t" + months[i]);
+        }
+
+        int minIndex = 0;
+        int maxIndex = 0;
+        for (int i = 0; i < rainfall.length; i++)
+        {
+            if (rainfall[i] < rainfall[minIndex])
+            {
+                minIndex = i;
+            }
+            else if (rainfall[i] > rainfall[maxIndex])
+            {
+                maxIndex = i;
+            }
+        }
+
+        println("Max rainfall: " + rainfall[maxIndex] + " in month " +months[maxIndex]);
+        println("Min rainfall: " + rainfall[minIndex] + " in month " +months[minIndex]);
+
+    }
+    public void draw(){
+
+
+    }
+    
+}
