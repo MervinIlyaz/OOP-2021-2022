043c872fdc66d56496e52b9daa7492e3e67ea815
MervinIlyaz <c20371681@mytudublin.ie>
2022-02-23 00:16:06 +0000

    bar chart

--- java/src/ie/tudublin/Arrays.java
+++ java/src/ie/tudublin/Arrays.java
@@ -1,44 +1,52 @@
 package ie.tudublin;
 
+import java.util.ArrayList;
+
 import processing.core.PApplet;
 
-public class Arrays extends PApplet{
-    
+public class Arrays extends PApplet {
 
-    float[] rainfall = {45,37,55,27,38,50,79,48,104,51,100,58};
 
-    String[] months = {"Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"};
-    
+    float[] rainfall = { 45, 37, 55, 27, 38, 50, 79, 48, 104, 31, 100, 58 };
+
+    String[] months = {"JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"};
+
     float[] a1 = new float[100];
-    float[] a2;   
-    public void settings(){
-        size(500,500);
+    float[] a2;
+
+    int minIndex = 0;
+    int maxIndex  = 0;
+        
+
+    public void settings()
+    {
+        size(500, 500);
     }
-    public void setup(){
-        for (int i= 0; i < rainfall.length; i++)
+
+    public void setup()
+    {
+        for(int i = 0 ; i < rainfall.length ; i ++)
         {
             println(rainfall[i] + "\t" + months[i]);
         }
-        for (float r:rainfall)
+        for(float r:rainfall)
         {
-            print(r);
+            println(r);
         }
-
+        
         int j = 0;
-        for (float r:rainfall)
+        for(float r:rainfall)
         {
             println(r + "\t" + months[j]);
-            j++;
+            j ++;
         }
 
-        for (int i = rainfall.length - 1; i >=0; i-- )
+        for(int i = rainfall.length - 1; i >= 0 ; i -- )
         {
             println(rainfall[i] + "\t" + months[i]);
         }
 
-        int minIndex = 0;
-        int maxIndex = 0;
-        for (int i = 0; i < rainfall.length; i++)
+        for(int i = 1 ; i < rainfall.length ; i ++)
         {
             if (rainfall[i] < rainfall[minIndex])
             {
@@ -47,27 +55,31 @@ public class Arrays extends PApplet{
             else if (rainfall[i] > rainfall[maxIndex])
             {
                 maxIndex = i;
-            }
+            } 
         }
 
-        println("Max rainfall: " + rainfall[maxIndex] + " in month " +months[maxIndex]);
-        println("Min rainfall: " + rainfall[minIndex] + " in month " +months[minIndex]);
+        println("Max rainfall: " + rainfall[maxIndex] + " in month " + months[maxIndex]);
+        println("Min rainfall: " + rainfall[minIndex] + " in month " + months[minIndex]);
 
     }
-    public void draw(){
+
+    public void draw()
+    {
+
         background(0);
         colorMode(HSB);
         float w = width / (float)rainfall.length;
         noStroke();
-        for (int i = 0; i < rainfall.length; i++)
+        for(int i = 0 ; i < rainfall.length ; i ++)
         {
             float x = map(i, 0, rainfall.length, 0, width);
             int c = (int)map(i, 0, rainfall.length, 0, 255);
             fill(c, 255, 255);
             float h = map(rainfall[i], 0, rainfall[maxIndex], 0, -height);
-            rect(x, height, w, -rainfall[i]);
+            rect(x, height, w, h);
+            fill(255);
             textAlign(CENTER, CENTER);
-            text(months, x + (w/2), height - 50);
+            text(months[i], x + (w / 2), height - 50);
         }
     }
     
