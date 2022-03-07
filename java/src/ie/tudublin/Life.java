90f4074233202cfd3ff6af3642ee25725f8f6151
MervinIlyaz <c20371681@mytudublin.ie>
2022-03-07 13:00:14 +0000

    life

--- java/src/ie/tudublin/Life.java
+++ java/src/ie/tudublin/Life.java
@@ -4,16 +4,23 @@ import processing.core.PApplet;
 
 public class Life extends PApplet {
 
+	LifeBoard board;
+	
 	public void settings() {
-		size(500, 500);
+		size(1000, 1000);
+		board = new LifeBoard(200, this);
+		board.randomise();
+
 	}
 
 	public void setup() {
-		colorMode(HSB);
+		colorMode(RGB);
 
 	}
 
 	
 	public void draw() {
+		board.render();
+		board.update();
 	}
 }
